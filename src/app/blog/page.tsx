d8c011086c369a66fdea3750f53a16da074f35ee
Swarup Bhise <swarupbhise420@gmail.com>
2026-01-01 16:49:56 +0530

    feat: Add First Blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,7 +15,7 @@ export default async function BlogPage() {
   return (
     <section>
       <BlurFade delay={BLUR_FADE_DELAY}>
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter">blog</h1>
+        <h1 className="font-medium text-2xl mb-8 tracking-tighter">Blogs</h1>
       </BlurFade>
       {posts
         .sort((a, b) => {
@@ -29,7 +29,7 @@ export default async function BlogPage() {
         .map((post, id) => (
           <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
             <Link
-              className="flex flex-col space-y-1 mb-4"
+              className="flex flex-col space-y-1 mb-4 border-gray-600 border-b-2"
               href={`/blog/${post.slug}`}
             >
               <div className="w-full flex flex-col">
